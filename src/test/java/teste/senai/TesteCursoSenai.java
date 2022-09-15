82dd81451f45ff7d385f1985fe3ef9ba2a2a486b
João <joao.manfron09@gmail.com>
2022-09-15 17:47:20 -0300

    testes do site eplayers

--- src/test/java/teste/senai/TesteCursoSenai.java
+++ src/test/java/teste/senai/TesteCursoSenai.java
@@ -6,27 +6,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TesteCursoSenai {
-	
-	private WebDriver driver;
-	
-	@Before
-	public void ConfigurarTeste() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\OI417408\\Desktop\\chromedriver\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-	}
-	
-	//Avisar o teste
-	@Test
-	public void TesteNavegabilidade () {
-		
-		driver.get("https://informatica.sp.senai.br/");
-		
-		//selecionar campo de teste
-		driver.findElement(By.id("Busca1_txtFiltro")).sendKeys("Software");
-		
-		driver.findElement(By.id("Busca1_btnBusca")).click();
-	}
+public class GameMania_testFront {
 
-}
+		private WebDriver driver;
+		
+		@Before
+		public void ConfigurarTeste() {
+			System.setProperty("webdriver.chrome.driver", "C:\\Program FIles\\chromedriver\\chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+		}
+
+		@Test
+		public void TesteNavegabilidade () {
+			driver.get("http://localhost:4200/");
+			driver.findElement(By.id("email")).sendKeys("algumacoisa@gmail.com");
+			driver.findElement(By.id("senha")).sendKeys("senha123");
+		}
+	}
